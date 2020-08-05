34ed38fba4e3ac00fd1d5c2beec2da2ad23d73f1
Himanshu Pandey <hpandey@pivotal.io>
2020-08-05 04:10:24 -0700

    Stack Implementation in Java

---
+++ MyStack.java
@@ -0,0 +1,65 @@
+package algorithmsAndDSnew;
+
+/**
+ * Basic Implementation of Stack Data Structure
+ * @author Himanshu
+ *
+ */
+public class MyStack {
+	
+	
+	Node top;
+	
+	/**
+	 *  inner class that holds the data as added to the stack
+	 * @author Himanshu
+	 *
+	 */
+	private class Node{
+		Node next =null;
+		Object data;
+		Node(Object o)
+		{
+			data = o;
+		}
+	}
+	
+	/**
+	 * Add the object to the Stack
+	 * @param o
+	 */
+	public void push(Object o)
+	{
+		Node n = new Node(o);
+		n.next = top;
+		top = n;
+	}
+	
+	/**
+	 * Removes object from the Stack
+	 * @return
+	 */
+	public Object pop()
+	{
+		if(top != null)
+		{
+			Object item = top.data;
+			top = top.next;
+			return item;
+			
+		}
+		return null;
+	}
+	
+	/**
+	 * Returns the top object from the Stack
+	 * @return
+	 */
+	public Object peek()
+	{	if(top == null)
+			return null;
+	
+	   return top.data;
+	}
+
+}
